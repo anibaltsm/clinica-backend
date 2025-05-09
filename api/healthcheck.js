df05d17fbdbe9eba43cfe8ab94a993f6c38e9de5
Anibal <anibal.tsm@hotmail.com>
2025-05-08 23:30:43 -0600

    Simplificar configuración de CORS en healthcheck

--- api/healthcheck.js
+++ api/healthcheck.js
@@ -1,13 +1,9 @@
 // Simple health check endpoint completely independent from Strapi
 module.exports = (req, res) => {
   // Set CORS headers to allow all origins
-  res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,POST,PUT,DELETE');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
   // Handle OPTIONS request
   if (req.method === 'OPTIONS') {
